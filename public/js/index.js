5dda6fd3a4f55aa66d2c278f05705ebc38b8eea0
TheDragoon69 <neutralbox420@gmail.com>
2019-02-17 16:11:31 +0200

    Git scroll to bottom if a new message appears

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,23 @@
 
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+        
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -23,7 +40,7 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(html);
 
 
-
+    scrollToBottom();
     // console.log('New message', message);
     // let li = $('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -43,6 +60,7 @@ socket.on('newLocationMessage', function (message) {
 
     jQuery('#messages').append(html);
 
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // let a = jQuery('<a target="_blank">My current location</a>');
     // li.text(`${message.from} ${formattedTime}: `);
